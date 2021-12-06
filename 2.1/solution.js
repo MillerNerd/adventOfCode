c4b02fb2b87b1ddd0f837ee004260517edf35647
millernerd <millernerd@gmail.com>
2021-12-05 21:51:48 -0700

    expanded attempt at one-line

--- 2.1/solution.js
+++ 2.1/solution.js
@@ -11,9 +11,24 @@ let findPositionVector = inputArray
         return { direction, magnitude }
     })
     .reduce((vector, curr, index, array) => {
-        if (curr.direction === 'forward') { vector.distance + curr.magnitude, vector.depth }
-        else if (curr.direction === 'down') { vector.distance, vector.depth + curr.magnitude }
-        else if (curr.direction === 'up') { vector.distance, vector.depth - curr.magnitude }
+        if (curr.direction === 'forward') { 
+            return {
+                'distance': vector.distance + curr.magnitude, 
+                'depth': vector.depth
+            }
+        }
+        else if (curr.direction === 'down') {
+            return {
+                'distance': vector.distance,
+                'depth': vector.depth + curr.magnitude
+            }
+        }
+        else if (curr.direction === 'up') {
+          return {
+                'distance': vector.distance, 
+                'depth': vector.depth - curr.magnitude
+            }
+        }
     }, { 'distance': 0, 'depth': 0 } )
 
 console.log(findPositionVector)
