6f2f6fb1659e9fedc2ea8bf7ef290995ff1dbc07
millernerd <millernerd@gmail.com>
2021-12-05 20:48:32 -0700

    1.2 solved

--- 1.2/solution.js
+++ 1.2/solution.js
@@ -1,10 +1,15 @@
 const fs = require('fs')
 
 const inputArray = fs.readFileSync('./input.txt', 'utf8')
+    .trimEnd()
     .split('\n')
     .map(Number)
 
-console.log(inputArray.reduce(countDepthIncreases, 0))
+let windowArray = inputArray
+    .map((element, index, array) => element + array[index + 1] + array[index + 2])
+    .slice(0, -2)
+
+console.log(windowArray.reduce(countDepthIncreases, 0))
 
 function countDepthIncreases(count, curr, index, array) {
     if (curr > array[index - 1]) {
