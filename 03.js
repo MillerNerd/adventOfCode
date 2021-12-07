8265fbc34329ff898049eefee524f58a1d72d45d
millernerd <millernerd@gmail.com>
2021-12-06 22:20:07 -0700

    decided array of strings was best

--- 03.js
+++ 03.js
@@ -1,12 +1,6 @@
 const fs = require('fs')
 
 let inputArray = fs.readFileSync('inputs/03.txt')
-  .map(Number)
-  .trimEnd()
-  .split('\n')
-
-let parseInput = string => string.split('')
-
-let bitArray = inputArray.map(parseInput)
-
-console.log(bitArray)
+	.toString()
+	.trimEnd()
+	.split('\n')
