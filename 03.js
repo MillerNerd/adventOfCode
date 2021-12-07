a1cb46f7a2c4cf22e10e32c237e22709fefa8d10
millernerd <millernerd@gmail.com>
2021-12-06 21:50:47 -0700

    started structuring input into 2D array

--- 03.js
+++ 03.js
@@ -1,6 +1,12 @@
 const fs = require('fs')
 
 let inputArray = fs.readFileSync('inputs/02.txt')
-  .toString()
+  .map(Number)
   .trimEnd()
   .split('\n')
+
+let parseInput = string => string.split('')
+
+let bitArray = inputArray.map(parseInput)
+
+console.log(bitArray)
