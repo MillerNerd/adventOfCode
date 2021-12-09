0de828f8e7abf6b5be8653fc1bdaf0a286892db7
millernerd <millernerd@gmail.com>
2021-12-09 15:17:01 -0700

    idk boo

--- 03.js
+++ 03.js
@@ -4,3 +4,11 @@ let inputArray = fs.readFileSync('inputs/03.txt')
 	.toString()
 	.trimEnd()
 	.split('\n')
+
+let calculateBitSum = string => {
+	
+}
+
+let bitSum = inputArray.reduce(
+	calculateBitSum, 
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
