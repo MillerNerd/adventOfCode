0b6f7e29f3c54d291f46bdb933e7091ae0ddc28f
millernerd <millernerd@gmail.com>
2021-12-09 16:13:06 -0700

    split input to 2D char array

--- 03.js
+++ 03.js
@@ -4,11 +4,29 @@ let inputArray = fs.readFileSync('inputs/03.txt')
 	.toString()
 	.trimEnd()
 	.split('\n')
+	.map( element => {
+		return element.split('')
+	})
 
-let calculateBitSum = string => {
-	
+console.log(inputArray)
+
+let rotateArray = (element, index, array) => {
+	let temp = element.split('')
 }
 
-let bitSum = inputArray.reduce(
-	calculateBitSum, 
-	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+// let calculateBitSum = (sumArray, curr) => {
+// 	return sumArray
+// 		.reduce( () => {
+// 			if (condition) {
+				
+// 			} else {
+				
+// 			}
+// 	})
+// }
+
+// let rotatedInputArray = inputArray.map(rotateArray)
+
+// let bitSum = inputArray.reduce(
+// 	calculateBitSum, 
+// 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
